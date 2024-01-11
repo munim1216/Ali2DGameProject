0d7bce683ad3160d0deeb8ee5f905841982bff1c
munim <munimali250@gmail.com>
2024-01-11 14:02:03 -0500

    working on array util class to keep null at the end of arrays

---
+++ src/ArrayUtil.java
@@ -0,0 +1,13 @@
+public class ArrayUtil {
+    private ArrayUtil(){}
+
+    public static SuperInteractable reorderArr(SuperInteractable[] arr) {
+        SuperInteractable[] newArr = new SuperInteractable[arr.length];
+        int nextAdded = 0;
+        for (SuperInteractable element : arr) {
+            if (element != null) {
+
+            }
+        }
+    }
+}
