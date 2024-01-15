aa07f9d65d0984c671c29300e3189384bde19cdf
owner <munimali250@gmail.com>
2024-01-15 11:46:15 -0500

    CUSTOM FONT WOOOOO

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -17,6 +17,7 @@ public class GamePanel extends JPanel implements Runnable {
     private Thread gameThread;
     public static int gameState;
     private final int FPS = 60;
+    public static final int TITLESCREEN = 3;
     public static final int DIAL0GUESTATE = 2;
     public static final int PLAYSTATE = 1;
     public static final int PAUSESTATE = 0;
@@ -37,54 +38,73 @@ public class GamePanel extends JPanel implements Runnable {
     AssetSetter assetSetter;
 
     public GamePanel() {
+        // setting up size of the window
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
 
-        gameState = PLAYSTATE;
+        // so you open on the title screen
+        gameState = TITLESCREEN;
 
+        // so the game can actually detect key input
         keyH = new KeyHandler(this);
         this.addKeyListener(keyH);
         this.setFocusable(true);
 
+        // our player
         player = new Player(keyH);
 
+        // needed to render only things in frame
         SuperInteractable.setPlayer(player);
 
-        ui = new UI(this, player);
+        // the ui,
+        ui = new UI(this);
 
+        // to deal with npcs (and maybe enemies in the future too)
         assetSetter = new AssetSetter();
         assetSetter.setNPC();
 
+        // to draw the tiles
         tileManager = new TileManager(this, player);
 
+        // collision hell needs all of these.
         Entity.setNeededVariables(tileManager.getMapTileNum(), tileManager.getTiles(), player, assetSetter.getNPCs(), this);
 
+        // the game can actually start if the thread is active
         startGameThread();
+
+        // the jframe
         setUpWindow();
+
+        // MUSIC THAT KINDA SUCKS
         MUSIC.setClip(0);
         MUSIC.play();
         MUSIC.loop();
     }
 
     private void startGameThread() {
+        // makes sure the game only updates and redraws every 1/60 of a second
         gameThread = new Thread(this);
         gameThread.start();
     }
 
     public void run() { // stupid code won't let me override it and reduce visibility
+        // variables needed to ensure its locked at 60 fps and below
         long currentTime = System.nanoTime();
         long previousTime = currentTime;
         double delta = 0.0;
         double drawInterval = 1000000000.0 / FPS;
 
         while (gameThread != null) {
+            // system.nanotime is java's very accurate clock or something (i dont 100% remember)
             currentTime = System.nanoTime();
 
+            // the time between now and the least time this looped
             delta += (double) (currentTime - previousTime) / drawInterval;
 
             previousTime = currentTime;
             if (delta >= 1) {
+                // delta being 1 or greater means 1/60 of a second;
                 update();
                 repaint();
                 delta = 0;
@@ -98,30 +118,19 @@ public class GamePanel extends JPanel implements Runnable {
       //  long drawStart = System.nanoTime();
 
         Graphics2D g2D = (Graphics2D) g;
-        if (gameState == PLAYSTATE) {
-            tileManager.draw(g2D);
-            SuperInteractable.draw(g2D);
-            assetSetter.draw(g2D);
-            ui.draw(g2D);
-            player.draw(g2D);
-        } else if (gameState == PAUSESTATE){
-            ui.drawPauseMenu(g2D);
-        } else if (gameState == DIAL0GUESTATE) {
-            tileManager.draw(g2D);
-            SuperInteractable.draw(g2D);
-            assetSetter.draw(g2D);
-            ui.draw(g2D);
-            ui.drawDialougeBox(g2D);
-            player.draw(g2D);
-        } else {
-            throw new UnsupportedOperationException();
+        switch (gameState) {
+            case PLAYSTATE -> playState(g2D);
+            case DIAL0GUESTATE -> dialogueState(g2D);
+            case PAUSESTATE -> pauseState(g2D);
+            case TITLESCREEN -> titleScreenState(g2D);
+            default -> throw new UnsupportedOperationException("HEY YOU'RE NOT SUPPOSED TO BE IN THIS STATE.");
         }
-
 
         // debugging
 //        long drawEnd = System.nanoTime();
 //        System.out.println("Draw Time: " + (drawEnd - drawStart));
 
+        // so we dont have a billion graphics 2d i think? (i dont remember the purpose of this i think its efficency or smth tho)
         g2D.dispose();
     }
 
@@ -141,6 +150,7 @@ public class GamePanel extends JPanel implements Runnable {
 
 
     private void update() {
+        // actually keeps the game runnning!
         player.playerUpdate();
         SuperInteractable.interactablesInFrame();
         assetSetter.update();
@@ -156,4 +166,28 @@ public class GamePanel extends JPanel implements Runnable {
         window.setVisible(true);
     }
 
+    private void playState(Graphics2D g2D) {
+        tileManager.draw(g2D);
+        SuperInteractable.draw(g2D);
+        assetSetter.draw(g2D);
+        ui.draw(g2D);
+        player.draw(g2D);
+    }
+
+    private void pauseState(Graphics2D g2D) {
+        ui.drawPauseMenu(g2D);
+    }
+
+    private void dialogueState(Graphics2D g2D) {
+        tileManager.draw(g2D);
+        SuperInteractable.draw(g2D);
+        assetSetter.draw(g2D);
+        ui.draw(g2D);
+        ui.drawDialougeBox(g2D);
+        player.draw(g2D);
+    }
+
+    private void titleScreenState(Graphics2D g2D) {
+        ui.drawTitleScreen(g2D);
+    }
 }
--- src/UI.java
+++ src/UI.java
@@ -1,9 +1,9 @@
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class UI {
     GamePanel gp;
-    Player player;
-
     private final int boxX;
     private final int width;
     private final int boxY;
@@ -14,9 +14,15 @@ public class UI {
     private String nextDialogue;
     private Font font;
 
-    public UI(GamePanel gp, Player player) {
+    public UI(GamePanel gp) {
         this.gp = gp;
-        this.player = player;
+
+        try { // credit to https://github.com/curadProgrammer/shorts-java-programs/blob/main/Adding%20Custom%20Font/src/App.java for helping me kinda understand how i can do it
+            File customFontFile = new File("src/Fonts/PixelEmulator-xq08.ttf");
+            font = Font.createFont(Font.TRUETYPE_FONT, customFontFile).deriveFont(24f);
+        } catch (FontFormatException | IOException e) { // i dont understand why intelliji is allowed to put one | instead of || for an or??? quite odd
+            e.printStackTrace();
+        }
 
         nextDialogue = "";
 
@@ -29,11 +35,11 @@ public class UI {
         textX = boxX + GamePanel.TILE_SIZE / 2;
         textY = boxY + GamePanel.TILE_SIZE;
 
-        font = new Font("Times New Roman", Font.BOLD, 20);
+
     }
 
     public void draw(Graphics2D g2D) {
-
+        // to be implemented later
     }
 
     public void drawPauseMenu(Graphics2D g2D) {
@@ -61,5 +67,9 @@ public class UI {
 
     public void setNextDialogue(String dialogue) {
         nextDialogue = dialogue;
+    }
+
+    public void drawTitleScreen(Graphics2D g2D) {
+
     }
 }
