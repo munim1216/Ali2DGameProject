94d705fdf6c418fd12461415e3409b1829ce4170
owner <munimali250@gmail.com>
2024-01-15 12:13:15 -0500

    added another custom font, and made what the title screen looks like, now to actually implement functionality

--- src/UI.java
+++ src/UI.java
@@ -1,4 +1,6 @@
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -18,7 +20,7 @@ public class UI {
         this.gp = gp;
 
         try { // credit to https://github.com/curadProgrammer/shorts-java-programs/blob/main/Adding%20Custom%20Font/src/App.java for helping me kinda understand how i can do it
-            File customFontFile = new File("src/Fonts/PixelEmulator-xq08.ttf");
+            File customFontFile = new File("src/Fonts/Cave-Story.ttf");
             font = Font.createFont(Font.TRUETYPE_FONT, customFontFile).deriveFont(24f);
         } catch (FontFormatException | IOException e) { // i dont understand why intelliji is allowed to put one | instead of || for an or??? quite odd
             e.printStackTrace();
@@ -70,6 +72,55 @@ public class UI {
     }
 
     public void drawTitleScreen(Graphics2D g2D) {
+        String text;
+        int x;
+        int y;
 
+        // TITLE OF GAME
+        g2D.setFont(font.deriveFont(125f));
+        g2D.setColor(Color.GRAY);
+        text = "REENE'S WORLD";
+        x = getXForCenteredText(text, g2D) + 3;
+        y = textY + GamePanel.TILE_SIZE + 3;
+
+        g2D.drawString(text, x, y);
+
+        x -= 3;
+        y -= 3;
+        g2D.setColor(Color.WHITE);
+        g2D.drawString(text, x, y);
+
+        // RENEE SPRITE
+        BufferedImage renee = null;
+        try {
+            renee = ImageIO.read(new File("src/sprites/ReneeSprite/reneeDown1.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        x = GamePanel.SCREEN_WIDTH / 2 - GamePanel.TILE_SIZE;
+        y = textY + GamePanel.TILE_SIZE * 2;
+        g2D.drawImage(renee, x, y, GamePanel.TILE_SIZE * 2, GamePanel.TILE_SIZE * 2, null);
+
+        // MENU
+        g2D.setFont(font.deriveFont(45f));
+        text = "NEW GAME";
+        x = getXForCenteredText(text, g2D);
+        y += GamePanel.TILE_SIZE * 4;
+        g2D.drawString(text, x, y);
+
+        text = "CONTINUE";
+        x = getXForCenteredText(text, g2D);
+        y += GamePanel.TILE_SIZE;
+        g2D.drawString(text, x, y);
+
+        text = "QUIT";
+        x = getXForCenteredText(text, g2D);
+        y += GamePanel.TILE_SIZE;
+        g2D.drawString(text, x, y);
+    }
+
+    private int getXForCenteredText(String text, Graphics2D g2D) {
+        int length = (int) g2D.getFontMetrics().getStringBounds(text, g2D).getWidth();
+        return GamePanel.SCREEN_WIDTH / 2 - length / 2;
     }
 }
